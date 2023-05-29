54dc6b30f395ff4d78680ba083bd20d96ad4443c
dinosoupy <anishbasoup@gmail.com>
2023-05-29 19:24:49 +1000

    Finished carousel component

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,26 +1,24 @@
 <template>
   <div class="relative isolate overflow-hidden bg-teal-700 py-24 sm:py-32">
-    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-9xl px-0 lg:px-0">
       <div class="mx-auto max-w-2xl lg:mx-auto text-center">
         <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Tell your story</h2>
         <p class="mt-6 text-lg leading-8 text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
       </div>
-      <div class="relative overflow-hidden">
-        <div class="flex w-full">
-          <div class="carousel-container">
-            <ul class="carousel-track">
-              <li v-for="(video, index) in videos" :key="index" class="carousel-item w-[500px] h-[300px] p-4 shrink-0">
-                <video autoplay loop muted :src="video.src" />
-              </li>
-            </ul>
-          </div>
-          <div class="carousel-container">
-            <ul class="carousel-track">
-              <li v-for="(video, index) in videos" :key="index" class="carousel-item w-[500px] h-[300px] p-4 shrink-0">
-                <video autoplay loop muted :src="video.src" />
-              </li>
-            </ul>
-          </div>
+      <div class="carousel-wrapper flex overflow-x-hidden pt-16 sm:pt-24">
+        <div class="tape shrink-0 p-0 grid grid-cols-1 relative w-full" ref="tape">
+          <ul class="flex flex-row flex-evenly list-none m-0 ps-0">
+            <li v-for="(video, index) in videos" :key="index" class="slot mr-6">
+              <video autoplay loop muted :src="video.src" class="rounded-lg"/>
+            </li>
+          </ul>
+        </div>
+        <div class="tape shrink-0 p-0 grid grid-cols-1 relative w-full" ref="tape">
+          <ul class="flex flex-row flex-evenly list-none m-0 ps-0">
+            <li v-for="(video, index) in videos" :key="index" class="slot mr-6">
+              <video autoplay loop muted :src="video.src" class="rounded-lg"/>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -32,53 +30,44 @@ export default {
   name: 'Carousel',
   data() {
     return {
-      videos: [
-        {
-          src: "../src/assets/video/apple-tv-video.mp4"
-        },
-        {
-          src: "../src/assets/video/apple-tv-video.mp4"
-        },
-        {
-          src: "../src/assets/video/apple-tv-video.mp4"
-        }
-      ]
+      videos: []
     };
   },
   mounted() {
-    const carouselContainer = document.querySelector('.carousel-container');
-    const carouselTrack = document.querySelector('.carousel-track');
-    const carouselItems = document.querySelectorAll('.carousel-item');
-    const itemWidth = 508; // 500px width + 4px padding (2px on each side)
-    const trackWidth = itemWidth * carouselItems.length;
+    this.setVideosArray(); // Set the initial videos array
+    window.addEventListener('resize', this.setVideosArray); // Update videos array on window resize
 
-    carouselTrack.style.width = `${trackWidth}px`;
-
-    const animation = carouselTrack.animate(
-      [
-        { transform: 'translateX(0)' },
-        { transform: `translateX(-${trackWidth}px)` }
-      ],
-      {
-        duration: 5000,
-        iterations: Infinity,
-        easing: 'linear'
+    const tapeWidth = this.$refs.tape.offsetWidth;
+    console.log(tapeWidth)
+    let moveTape = anime({
+      targets: '.tape',
+      translateX: -tapeWidth,
+      duration: 5000,
+      easing: 'linear',
+      loop: true
+    });
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.setVideosArray); // Remove event listener on component destroy
+  },
+  methods: {
+    setVideosArray() {
+      // Check the screen width and set the videos array accordingly
+      if (window.innerWidth < 768) {
+        this.videos = [
+          { src: "../src/assets/video/apple-tv-video.mp4" },
+          { src: "../src/assets/video/apple-tv-video.mp4" },
+        ];
+      } else {
+        this.videos = [
+          { src: "../src/assets/video/apple-tv-video.mp4" },
+          { src: "../src/assets/video/apple-tv-video.mp4" },
+          { src: "../src/assets/video/apple-tv-video.mp4" },
+          { src: "../src/assets/video/apple-tv-video.mp4" },
+        ];
       }
-    );
-
-    animation.play();
+    }
   }
 }
+
 </script>
-
-<style scoped>
-.carousel-container {
-  overflow: hidden;
-  width: 100%;
-}
-
-.carousel-track {
-  display: flex;
-  transition: transform 0.5s ease;
-}
-</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Hero />
+  <Header heading="YOYOYO" subheading="Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua." />
+  <LogoCloud />
+  <Header heading="What we bring to the table" subheading="" />
+  <FeatureGrid />
+  <Header heading="We get results" />
+  <Stats />
+  <Carousel />
+  <Header heading="Questions? Answers." subheading="" />
+  <Faq />
+  <Footer />
 </template>
 
-<style scoped>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script>
+import Hero from '@/components/Hero.vue'
+import LogoCloud from '@/components/LogoCloud.vue'
+import Carousel from '@/components/Carousel.vue'
+import Header from '@/components/Header.vue'
+import Stats from '@/components/Stats.vue'
+import FeatureGrid from '@/components/FeatureGrid.vue'
+import Faq from '@/components/Faq.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Hero,
+    LogoCloud,
+    Carousel,
+    Header,
+    Stats,
+    FeatureGrid,
+    Faq,
+    Footer
   }
 }
-</style>
+</script>
